afd3fa086c42c1b7fd8efc8bda6dee9334463a29
Peter Poole <peter.j.poole@gmail.com>
2015-11-24 00:24:40 -0500

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124052135) do
+ActiveRecord::Schema.define(version: 20151124052357) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "accounts", ["user_id"], name: "index_accounts_on_user_id"
 
   create_table "statements", force: :cascade do |t|
     t.date     "start_date"
@@ -28,5 +31,11 @@ ActiveRecord::Schema.define(version: 20151124052135) do
   end
 
   add_index "statements", ["account_id"], name: "index_statements_on_account_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
