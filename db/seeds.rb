b399e7b9f2afe621903f2c9186e06b667cce9d00
Peter Poole <peter.j.poole@gmail.com>
2015-12-20 13:32:37 -0500

    Seeds for drop/create

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create(name: "Peter")
+
+%w(Checking Savings Credit Cash).each do |type|
+  user.accounts.create(name: type)
+end
