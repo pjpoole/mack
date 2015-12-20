3617bedded495cb621235fcbfaaacb8f0b87125f
Peter Poole <peter.j.poole@gmail.com>
2015-12-20 13:32:58 -0500

    Basic index route

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,4 +3,8 @@ class AccountsController < ApplicationController
     @account = Account.includes(:transactions).find(params[:id])
     @transaction = Transaction.new
   end
+
+  def index
+    @accounts = Account.all
+  end
 end
